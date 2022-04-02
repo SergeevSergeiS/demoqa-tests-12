cf9d9d5fb7ace15dc78a11408c5ed2dd006f21bb
SergeevSS90 <sergeevss90@internet.ru>
2022-04-02 12:34:35 +0400

    build.gradle is updated

---
+++ src/test/java/ru/internet/sergeevss90/RegFormTest.java
@@ -0,0 +1,24 @@
+package ru.internet.sergeevss90;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.lang.module.Configuration;
+
+public class RegFormTest {
+
+    @BeforeAll
+    static void prepare() {
+        Configuration.
+    }
+
+    @Test
+    void execute() {
+
+    }
+}
+
+/*
+ * Блочный комментарий
+ */
